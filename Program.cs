724383f0a18399123b47ca422946a8a331b2a01b
WIDYAN IRWADI <015021@npid.com>
2023-06-12 15:33:17 +0700

    initial commit of LearnAsync Project

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace LearnAsync
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Main method started");
+
+            // Memanggil metode asinkron
+            await PerformTaskAsync();
+
+            Console.WriteLine("Main method completed");
+        }
+
+        static async Task PerformTaskAsync()
+        {
+            Console.WriteLine("Performing a long-running task...");
+            await Task.Delay(2000); // Menunggu selama 2 detik (simulasi tugas asinkron)
+
+            Console.WriteLine("Task completed");
+        }
+        
+    }
+}
